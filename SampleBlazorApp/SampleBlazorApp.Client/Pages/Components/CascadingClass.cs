1ef6bbdde2126abbc4db6ad51bca86c979e6db74
Saleh Yusefnejad <msynk@outlook.com>
2021-02-16 13:39:30 +0330

    3rd Session

---
+++ SampleBlazorApp/SampleBlazorApp.Client/Pages/Components/CascadingClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleBlazorApp.Client.Pages.Components
+{
+    public class CascadingClass: ICascadingClass
+    {
+        public string Prop1 { get; set; }
+        public int Prop2 { get; set; }
+    }
+}
